94a82f2edd16ff16870d197690d4c8684d159e35
LX-KI <rlaukeek@gmail.com>
2024-08-31 02:06:32 +0530

    5x5 square of *'s

---
+++ Star_rec.java
@@ -0,0 +1,12 @@
+
+public class Star_rec {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
