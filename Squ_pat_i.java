6973fbc092a04a774a8aaf3234b775feb894c119
LX-KI <rlaukeek@gmail.com>
2024-08-31 03:19:09 +0530

    a square 5x5 pattern where each row prints the value of i : 11111 22222 33333 44444 55555

---
+++ Squ_pat_i.java
@@ -0,0 +1,11 @@
+public class Squ_pat_i {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j <= rows; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
