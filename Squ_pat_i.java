741fa4d3717aa9cf45d99645bea85d912143e6ee
LX-KI <rlaukeek@gmail.com>
2024-08-31 03:39:35 +0530

    edit

--- Squ_pat_i.java
+++ Squ_pat_i.java
@@ -1,7 +1,7 @@
 public class Squ_pat_i {
     public static void main(String[] args) {
         int rows = 5;
-        for (int i = 1; i < rows; i++) {
+        for (int i = 1; i <=rows; i++) {
             for (int j = 1; j <= rows; j++) {
                 System.out.print(i + " ");
             }
