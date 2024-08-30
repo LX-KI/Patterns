57121a031e01764276626a7ab056ae16959c7bd4
LX-KI <rlaukeek@gmail.com>
2024-08-31 02:26:37 +0530

    A square 5x5 pattern containing number from 1 to 5

---
+++ Num_rec.java
@@ -0,0 +1,12 @@
+public class Num_rec {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
