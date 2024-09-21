7cb200922eaba95d9c89598d9cead05a5a078be2
LX-KI <rlaukeek@gmail.com>
2024-09-21 23:47:50 +0530

    Repeated number triangle

---
+++ Tri_1_5.java
@@ -0,0 +1,11 @@
+public class Tri_1_5 {
+public static void main(String[] args) {
+    int rows =5;
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            System.out.print(i+" ");
+        }    
+        System.out.println();
+    }    
+}
+}
