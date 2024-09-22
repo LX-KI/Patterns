ae94c7f80aaffdfc39675ed5b1cf9aca24c0c30b
LX-KI <rlaukeek@gmail.com>
2024-09-22 17:29:20 +0530

    Right aligned start pattern

---
+++ bot_ryt_tri.java
@@ -0,0 +1,17 @@
+public class bot_ryt_tri {
+public static void main(String[] args) {
+    int rows = 5;
+    for(int i=1;i<=rows;i++)
+    {
+        for(int j=1;j<=rows-i;j++)
+        {
+            System.out.print(" ");
+        }
+        for(int k=1;k<=i;k++)
+        {
+            System.out.print("*");
+            }
+        System.out.println();
+    }
+}
+}
