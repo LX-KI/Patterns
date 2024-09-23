6a81551fd2186690f98c69ba24e7a67c41c7dd30
LX-KI <rlaukeek@gmail.com>
2024-09-24 01:50:45 +0530

    A pyramid pattern of 121

---
+++ Pyramid_nums_121.java
@@ -0,0 +1,20 @@
+public class Pyramid_nums_121 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            // Spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" " + " ");
+            }
+            // Numbers part1
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k + " ");
+            }
+            for (int k = i - 1; k >= 1; k--) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+    }
+}
