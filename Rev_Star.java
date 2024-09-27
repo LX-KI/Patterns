692c860dbb43278ac2e84997e41ff25af51b24aa
LX-KI <rlaukeek@gmail.com>
2024-09-27 23:58:00 +0530

    Reversed Pyramid of stars

---
+++ Rev_Star.java
@@ -0,0 +1,17 @@
+public class Rev_Star {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i - 1; j++) {
+                System.out.print(" " + " ");
+            }
+            for (int j = 1; j <= n + 1 - i; j++) {
+                System.out.print("*" + " ");
+            }
+            for (int j = n - i; j >= 1; j--) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
