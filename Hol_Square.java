f1981dad089557336d74d41403969ad116471f44
LX-KI <rlaukeek@gmail.com>
2024-09-28 00:08:27 +0530

    Hollow pattern of stars in a square shape

---
+++ Hol_Square.java
@@ -0,0 +1,17 @@
+public class Hol_Square {
+    public static void main(String[] args) {
+
+        int rows=5;
+        for(int i=1;i<=rows;i++) {
+            for(int j=1;j<=rows;j++) {
+                if(i==1 || i==5 || j==1 || j==5) {
+                    System.out.print("*"+ " ");
+                }
+                else {
+                    System.out.print(" "+" ");
+                }
+            }
+            System.out.println();
+        }
+    }    
+}
