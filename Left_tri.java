218357ad1c03706adadad9508d017ec5e7de7143
LX-KI <rlaukeek@gmail.com>
2024-10-02 23:41:14 +0530

    top left tri of stars patterns

--- Left_tri.java
+++ Left_tri.java
@@ -1,9 +1,9 @@
 public class Left_tri {
     public static void main(String[] args) {
-        int rows = 5;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= rows-i+1; j++) {
-                System.out.print("*"+ " ");
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("*" + " ");
             }
             System.out.println();
         }
